8d3f521a33549bafe470efc4a943d153503cde31
trushant05 <trushant@umich.edu>
2025-07-26 10:51:08 -0700

    [add] robotrail color theme

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# sphinx_book_theme
-Sphinx book theme for documentation
+# Sphinx Book Theme Template
+
+A customizable documentation template using the **Sphinx Book Theme**, designed for readable, navigable, and professionally styled docs. This template is suitable for research projects, open-source libraries, robotics stacks, or any technical documentation.
+
+
+## Table of Contents
+
+1. [Key Features](#key-features)
+2. [Getting Started](#getting-started)
+3. [Version Dependency](#version-dependency)
+4. [Prerequisites](#prerequisites)
+5. [License](#license)
+6. [Citation](#citation)
+
+
+## Key Features
+
+- **Book-style layout** optimized for long-form documentation
+- Supports **Jupyter notebooks**, **Markdown**, and **reStructuredText**
+- **Automatic sidebar generation**
+- Includes a **custom theme** (light and dark mode)
+- Ready for **API docs** with autodoc/autosummary support
+- Easily deployable with **GitHub Pages**
+- Modular folder structure
+
+
+## Getting Started
+
+## Version Dependency
+
+## Prerequisites 
+
+## License
+
+This project is licensed under the **MIT License**, a short and permissive open-source license.
+
+You are free to:
+  - Use the code and documentation for personal or commercial purposes
+  - Modify and adapt it to your needs
+  - Distribute your changes under the same license terms
+
+**Attribution is appreciated but not required.**  
+You can read the full license text in the [LICENSE](LICENSE) file.
+
+
+## Citation
+
+If you use Sphinx Book Theme Template, please consider citing:
+
+```bibtex
+@misc{Sphinx Book Theme Template,
+    author = {Sphinx Book Theme Template Authors},
+    title = {Sphinx Book Theme Template},
+    month = {July},
+    year = {2025},
+    url = {https://github.com/trushant05/sphinx_book_theme_template}
+}
--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+import os
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,31 +8,73 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Sphinx Book Theme'
+project = 'Sphinx Book Theme Template'
 copyright = '2025, Trushant Adeshara'
 author = 'Trushant Adeshara'
+
+# Read verson from the package
+with open(os.path.join(os.path.dirname(__file__), "..", "VERSION")) as f:
+    full_version = f.read().strip()
+    version = ".".join(full_version.split(".")[:3])
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming ith Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
     "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
 ]
 
+# Supported file extensions for source files
 source_suffix = {
     '.rst': "restructuredtext",
     '.md': "markdown",
 }
 
+# Add any paths that contain templates here, realative to this directory.
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# List of patterns, relative to source directory, that match file and 
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_paths.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "README.md"]
 
+# -- Internationalization ----------------------------------------------------
+
+# specifying the natural language populates some key tags
+language = "en"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
-html_static_path = ['_static']
+import sphinx_book_theme
+
+html_title = "Sphinx Book Theme Template"
+html_theme_path = [sphinx_book_theme.get_html_theme_path()]
+html_theme = "sphinx_book_theme"
+#TODO Add logo of the project
+#html_favicon = "source/_static/favicon.ico"
+html_show_copyright = True
+html_show_sphinx = False
+html_last_updated_fmt = "" # To reveal the build data in the pages meta
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are coped after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["source/_static/css"]
+html_css_files = ["custom.css"]
+
+html_theme_options = {
+    "path_to_docs": "docs/",
+    "collapse_navigation": True,
+    "repository_url": "https://github.com/trushant05/sphinx_book_theme_template",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "show_toc_level": 1,
+    "use_sidenotes": True,
+}
